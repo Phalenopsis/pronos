fb281897a7ebc9526c530d0f8602fc9beb9c841b
Nicolas Leroux <hertford.fr@gmail.com>
2024-02-01 19:14:53 +0100

    form to change game result

---
+++ src/Form/ResultType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Game;
+use App\Entity\Team;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ResultType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('result', ChoiceType::class, [
+                'choices' => [
+                    'Equipe 1' => '1',
+                    'Equipe 2' => '2',
+                    'Match nul' => 0
+                ],
+                'multiple' => false,
+                'expanded' => true,
+                'by_reference' => false,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Game::class,
+        ]);
+    }
+}
