5f8b17fe700123d9079b36adf9160ccef9a30bcf
Nicolas Leroux <hertford.fr@gmail.com>
2024-02-01 11:23:00 +0100

    make:entity Game

---
+++ src/Entity/Game.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\GameRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: GameRepository::class)]
+class Game
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'homegames')]
+    private ?Team $team1 = null;
+
+    #[ORM\ManyToOne(inversedBy: 'movegames')]
+    private ?Team $team2 = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $result = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTeam1(): ?Team
+    {
+        return $this->team1;
+    }
+
+    public function setTeam1(?Team $team1): static
+    {
+        $this->team1 = $team1;
+
+        return $this;
+    }
+
+    public function getTeam2(): ?Team
+    {
+        return $this->team2;
+    }
+
+    public function setTeam2(?Team $team2): static
+    {
+        $this->team2 = $team2;
+
+        return $this;
+    }
+
+    public function getResult(): ?string
+    {
+        return $this->result;
+    }
+
+    public function setResult(string $result): static
+    {
+        $this->result = $result;
+
+        return $this;
+    }
+}
--- src/Entity/Team.php
+++ src/Entity/Team.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\TeamRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: TeamRepository::class)]
@@ -24,6 +26,18 @@ class Team
 
     #[ORM\Column(length: 255)]
     private ?string $color3 = null;
+
+    #[ORM\OneToMany(mappedBy: 'team1', targetEntity: Game::class)]
+    private Collection $homegames;
+
+    #[ORM\OneToMany(mappedBy: 'team2', targetEntity: Game::class)]
+    private Collection $movegames;
+
+    public function __construct()
+    {
+        $this->homegames = new ArrayCollection();
+        $this->movegames = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -74,6 +88,66 @@ class Team
     public function setColor3(string $color3): static
     {
         $this->color3 = $color3;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Game>
+     */
+    public function getHomegames(): Collection
+    {
+        return $this->homegames;
+    }
+
+    public function addHomegame(Game $homegame): static
+    {
+        if (!$this->homegames->contains($homegame)) {
+            $this->homegames->add($homegame);
+            $homegame->setTeam1($this);
+        }
+
+        return $this;
+    }
+
+    public function removeHomegame(Game $homegame): static
+    {
+        if ($this->homegames->removeElement($homegame)) {
+            // set the owning side to null (unless already changed)
+            if ($homegame->getTeam1() === $this) {
+                $homegame->setTeam1(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Game>
+     */
+    public function getMovegames(): Collection
+    {
+        return $this->movegames;
+    }
+
+    public function addMovegame(Game $movegame): static
+    {
+        if (!$this->movegames->contains($movegame)) {
+            $this->movegames->add($movegame);
+            $movegame->setTeam2($this);
+        }
+
+        return $this;
+    }
+
+    public function removeMovegame(Game $movegame): static
+    {
+        if ($this->movegames->removeElement($movegame)) {
+            // set the owning side to null (unless already changed)
+            if ($movegame->getTeam2() === $this) {
+                $movegame->setTeam2(null);
+            }
+        }
 
         return $this;
     }
