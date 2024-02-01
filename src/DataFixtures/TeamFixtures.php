9e81a1558ffd6f2d193af706d9a9bb846e5adb8d
Nicolas Leroux <hertford.fr@gmail.com>
2024-02-01 12:29:25 +0100

    add slug and reference

--- src/DataFixtures/TeamFixtures.php
+++ src/DataFixtures/TeamFixtures.php
@@ -7,10 +7,18 @@ use App\Service\ColorGenerator;
 use App\Service\TeamNameGenerator;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
 ;
 
 class TeamFixtures extends Fixture
 {
+    private SluggerInterface $slugger;
+
+    public function __construct(SluggerInterface $slugger)
+    {
+        $this->slugger = $slugger;
+    }
     public function load(ObjectManager $manager): void
     {
         $colorGenerator = new ColorGenerator();
@@ -26,7 +34,10 @@ class TeamFixtures extends Fixture
             $team->setColor1($colorGenerator->generate());
             $team->setColor2($colorGenerator->generate());
             $team->setColor3($colorGenerator->generate());
+
+            $team->setSlug($this->slugger->slug($name));
             $manager->persist($team);
+            $this->addReference('team_' . $i, $team);
         }
         // $product = new Product();
         // $manager->persist($product);
