d945c9699171274100fd7db410f7ebc96194551d
Nicolas Leroux <hertford.fr@gmail.com>
2024-02-01 19:29:09 +0100

    move crud to admin

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -2,12 +2,8 @@
 
 namespace App\Controller;
 
-use App\Entity\Game;
-use App\Form\GameType;
 use App\Repository\GameRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -32,60 +28,4 @@ class GameController extends AbstractController
         ]);
     }
 
-    #[Route('/new', name: 'app_game_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $game = new Game();
-        $form = $this->createForm(GameType::class, $game);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($game);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_game_crud_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('game_crud/new.html.twig', [
-            'game' => $game,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_game_show', methods: ['GET'])]
-    public function show(Game $game): Response
-    {
-        return $this->render('game_crud/show.html.twig', [
-            'game' => $game,
-        ]);
-    }
-
-    #[Route('/{id}/edit', name: 'app_game_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Game $game, EntityManagerInterface $entityManager): Response
-    {
-        $form = $this->createForm(GameType::class, $game);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_game_crud_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('game_crud/edit.html.twig', [
-            'game' => $game,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_game_delete', methods: ['POST'])]
-    public function delete(Request $request, Game $game, EntityManagerInterface $entityManager): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$game->getId(), $request->request->get('_token'))) {
-            $entityManager->remove($game);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('app_game', [], Response::HTTP_SEE_OTHER);
-    }
 }
