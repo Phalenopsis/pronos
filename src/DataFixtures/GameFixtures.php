631bdfc0934cbc6e0889f75c0143cce165a0af94
Nicolas Leroux <hertford.fr@gmail.com>
2024-02-01 12:29:56 +0100

    add fixtures for games

---
+++ src/DataFixtures/GameFixtures.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Game;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+;
+
+class GameFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function getDependencies(): array
+    {
+        return [
+            TeamFixtures::class
+        ];
+    }
+    public function load(ObjectManager $manager): void
+    {
+        for ($i = 0; $i < 10; $i += 1) {
+            $game = new Game();
+            $team1Nb = rand(0, 9);
+            do{
+                $team2Nb = rand(0,9);
+            } while ($team2Nb === $team1Nb);
+            $game->setTeam1($this->getReference('team_' . $team1Nb));
+            $game->setTeam2($this->getReference('team_' . $team2Nb));
+            $game->setResult(rand(0,2));
+            $manager->persist($game);
+        }
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $manager->flush();
+    }
+}
