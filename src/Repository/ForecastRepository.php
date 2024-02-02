26a59cf50515e580a9af40c7ea9e6de135345ef3
Nicolas Leroux <hertford.fr@gmail.com>
2024-02-02 09:08:57 +0100

    method to find all forecasts for a game

--- src/Repository/ForecastRepository.php
+++ src/Repository/ForecastRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Forecast;
+use App\Entity\Game;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -19,6 +20,15 @@ class ForecastRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Forecast::class);
+    }
+
+    public function getAllWhereGameIs(Game $game): int
+    {
+        return $this->createQueryBuilder('f')
+            ->andWhere('f.game = :val')
+            ->setParameter('val', $game->getId())
+            ->getQuery()
+            ->getResult();
     }
 
 //    /**
